19117be5421fef3b4b6ea8863a2535aee087b1be
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-05-25 16:36:46 +0200

    don't use pointers in arrays; test with jsonapi marshalling

--- buildLog_test.go
+++ buildLog_test.go
@@ -1,10 +1,12 @@
 package contracts
 
 import (
+	"bytes"
 	"encoding/json"
 	"testing"
 	"time"
 
+	"github.com/google/jsonapi"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,8 +21,8 @@ func TestBuildLog(t *testing.T) {
 			RepoName:     "estafette-ci-api",
 			RepoBranch:   "master",
 			RepoRevision: "as23456",
-			Steps: []*BuildLogStep{
-				&BuildLogStep{
+			Steps: []BuildLogStep{
+				BuildLogStep{
 					Step: "init",
 					Image: &BuildLogStepDockerImage{
 						Name:         "golang",
@@ -30,12 +32,12 @@ func TestBuildLog(t *testing.T) {
 						PullDuration: 2 * time.Second,
 						Error:        "",
 					},
-					Commands: []*BuildLogStepCommand{
-						&BuildLogStepCommand{
+					Commands: []BuildLogStepCommand{
+						BuildLogStepCommand{
 							Command:  "go test -v `go list ./... | grep -v /vendor/`",
 							Duration: 91 * time.Second,
-							LogLines: []*BuildLogLine{
-								&BuildLogLine{
+							LogLines: []BuildLogLine{
+								BuildLogLine{
 									Timestamp:  time.Date(2018, 4, 17, 8, 3, 0, 0, time.UTC),
 									StreamType: "stdout",
 									Text: "ok  	github.com/estafette/estafette-ci-contracts	0.017s",
@@ -56,4 +58,54 @@ func TestBuildLog(t *testing.T) {
 		assert.Nil(t, err)
 		assert.Equal(t, "{\"id\":\"5\",\"repoSource\":\"github.com\",\"repoOwner\":\"estafette\",\"repoName\":\"estafette-ci-api\",\"repoBranch\":\"master\",\"repoRevision\":\"as23456\",\"steps\":[{\"step\":\"init\",\"image\":{\"name\":\"golang\",\"tag\":\"1.10.2-alpine3.7\",\"isPulled\":false,\"imageSize\":135000,\"pullDuration\":2000000000},\"commands\":[{\"command\":\"go test -v `go list ./... | grep -v /vendor/`\",\"duration\":91000000000,\"logLines\":[{\"timestamp\":\"2018-04-17T08:03:00Z\",\"streamType\":\"stdout\",\"text\":\"ok  \\tgithub.com/estafette/estafette-ci-contracts\\t0.017s\"}]}],\"exitCode\":0,\"status\":\"SUCCEEDED\"}],\"insertedAt\":\"2018-04-17T08:03:00Z\"}", string(bytes))
 	})
+
+	t.Run("JSONAPIMarshalPayloadSingleBuildLog", func(t *testing.T) {
+
+		buildLog := BuildLog{
+			ID:           "5",
+			RepoSource:   "github.com",
+			RepoOwner:    "estafette",
+			RepoName:     "estafette-ci-api",
+			RepoBranch:   "master",
+			RepoRevision: "as23456",
+			Steps: []BuildLogStep{
+				BuildLogStep{
+					Step: "init",
+					Image: &BuildLogStepDockerImage{
+						Name:         "golang",
+						Tag:          "1.10.2-alpine3.7",
+						IsPulled:     false,
+						ImageSize:    135000,
+						PullDuration: 2 * time.Second,
+						Error:        "",
+					},
+					Commands: []BuildLogStepCommand{
+						BuildLogStepCommand{
+							Command:  "go test -v `go list ./... | grep -v /vendor/`",
+							Duration: 91 * time.Second,
+							LogLines: []BuildLogLine{
+								BuildLogLine{
+									Timestamp:  time.Date(2018, 4, 17, 8, 3, 0, 0, time.UTC),
+									StreamType: "stdout",
+									Text: "ok  	github.com/estafette/estafette-ci-contracts	0.017s",
+								},
+							},
+						},
+					},
+					ExitCode: 0,
+					Status:   "SUCCEEDED",
+				},
+			},
+			InsertedAt: time.Date(2018, 4, 17, 8, 3, 0, 0, time.UTC),
+		}
+
+		b := new(bytes.Buffer)
+
+		// act
+		err := jsonapi.MarshalPayload(b, &buildLog)
+
+		assert.Nil(t, err)
+		assert.Equal(t, "{\"data\":{\"type\":\"build-logs\",\"id\":\"5\",\"attributes\":{\"inserted-at\":1523952180,\"repo-branch\":\"master\",\"repo-name\":\"estafette-ci-api\",\"repo-owner\":\"estafette\",\"repo-revision\":\"as23456\",\"repo-source\":\"github.com\",\"steps\":[{\"step\":\"init\",\"image\":{\"name\":\"golang\",\"tag\":\"1.10.2-alpine3.7\",\"isPulled\":false,\"imageSize\":135000,\"pullDuration\":2000000000},\"commands\":[{\"command\":\"go test -v `go list ./... | grep -v /vendor/`\",\"duration\":91000000000,\"logLines\":[{\"timestamp\":\"2018-04-17T08:03:00Z\",\"streamType\":\"stdout\",\"text\":\"ok  \\tgithub.com/estafette/estafette-ci-contracts\\t0.017s\"}]}],\"exitCode\":0,\"status\":\"SUCCEEDED\"}]}}}\n", b.String())
+	})
+
 }
