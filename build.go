28ac7a5b2c9b97a8c357826b95cb41cb207cb709
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-06-25 15:22:29 +0200

    add labels and commits to build and pipeline structs

--- build.go
+++ build.go
@@ -4,16 +4,17 @@ import "time"
 
 // Build represents a specific build, including version number, repo, branch, revision, labels and manifest
 type Build struct {
-	ID           string    `json:"id"`
-	RepoSource   string    `json:"repoSource"`
-	RepoOwner    string    `json:"repoOwner"`
-	RepoName     string    `json:"repoName"`
-	RepoBranch   string    `json:"repoBranch"`
-	RepoRevision string    `json:"repoRevision"`
-	BuildVersion string    `json:"buildVersion"`
-	BuildStatus  string    `json:"buildStatus"`
-	Labels       string    `json:"labels"`
-	Manifest     string    `json:"manifest"`
-	InsertedAt   time.Time `json:"insertedAt"`
-	UpdatedAt    time.Time `json:"updatedAt"`
+	ID           string      `json:"id"`
+	RepoSource   string      `json:"repoSource"`
+	RepoOwner    string      `json:"repoOwner"`
+	RepoName     string      `json:"repoName"`
+	RepoBranch   string      `json:"repoBranch"`
+	RepoRevision string      `json:"repoRevision"`
+	BuildVersion string      `json:"buildVersion"`
+	BuildStatus  string      `json:"buildStatus"`
+	Labels       []Label     `json:"labels"`
+	Manifest     string      `json:"manifest"`
+	Commits      []GitCommit `json:"commits"`
+	InsertedAt   time.Time   `json:"insertedAt"`
+	UpdatedAt    time.Time   `json:"updatedAt"`
 }
--- pipeline_test.go
+++ pipeline_test.go
@@ -21,17 +21,40 @@ func TestPipeline(t *testing.T) {
 			RepoRevision: "as23456",
 			BuildVersion: "1.0.0",
 			BuildStatus:  "succeeded",
-			Labels:       "",
-			Manifest:     "",
-			InsertedAt:   time.Date(2018, 4, 17, 8, 3, 0, 0, time.UTC),
-			UpdatedAt:    time.Date(2018, 4, 17, 8, 15, 0, 0, time.UTC),
+			Labels: []Label{
+				Label{
+					Key:   "app",
+					Value: "estafette-ci-api",
+				},
+				Label{
+					Key:   "team",
+					Value: "estafette-team",
+				},
+				Label{
+					Key:   "language",
+					Value: "golang",
+				},
+			},
+			Manifest: "",
+			Commits: []GitCommit{
+				GitCommit{
+					Message: "First commit",
+					Author: GitAuthor{
+						Email:    "name@server.com",
+						Name:     "Name",
+						Username: "MyName",
+					},
+				},
+			},
+			InsertedAt: time.Date(2018, 4, 17, 8, 3, 0, 0, time.UTC),
+			UpdatedAt:  time.Date(2018, 4, 17, 8, 15, 0, 0, time.UTC),
 		}
 
 		// act
 		bytes, err := json.Marshal(&pipeline)
 
 		assert.Nil(t, err)
-		assert.Equal(t, "{\"id\":\"5\",\"repoSource\":\"github.com\",\"repoOwner\":\"estafette\",\"repoName\":\"estafette-ci-api\",\"repoBranch\":\"master\",\"repoRevision\":\"as23456\",\"buildVersion\":\"1.0.0\",\"buildStatus\":\"succeeded\",\"labels\":\"\",\"manifest\":\"\",\"insertedAt\":\"2018-04-17T08:03:00Z\",\"updatedAt\":\"2018-04-17T08:15:00Z\"}", string(bytes))
+		assert.Equal(t, "{\"id\":\"5\",\"repoSource\":\"github.com\",\"repoOwner\":\"estafette\",\"repoName\":\"estafette-ci-api\",\"repoBranch\":\"master\",\"repoRevision\":\"as23456\",\"buildVersion\":\"1.0.0\",\"buildStatus\":\"succeeded\",\"labels\":[{\"key\":\"app\",\"value\":\"estafette-ci-api\"},{\"key\":\"team\",\"value\":\"estafette-team\"},{\"key\":\"language\",\"value\":\"golang\"}],\"manifest\":\"\",\"commits\":[{\"message\":\"First commit\",\"author\":{\"email\":\"name@server.com\",\"name\":\"Name\",\"username\":\"MyName\"}}],\"insertedAt\":\"2018-04-17T08:03:00Z\",\"updatedAt\":\"2018-04-17T08:15:00Z\"}", string(bytes))
 	})
 
 	t.Run("JSONMarshalPayloadArrayOfPipelines", func(t *testing.T) {
@@ -47,10 +70,33 @@ func TestPipeline(t *testing.T) {
 			RepoRevision: "as23456",
 			BuildVersion: "1.0.0",
 			BuildStatus:  "succeeded",
-			Labels:       "",
-			Manifest:     "",
-			InsertedAt:   time.Date(2018, 4, 17, 8, 3, 0, 0, time.UTC),
-			UpdatedAt:    time.Date(2018, 4, 17, 8, 15, 0, 0, time.UTC),
+			Labels: []Label{
+				Label{
+					Key:   "app",
+					Value: "estafette-ci-api",
+				},
+				Label{
+					Key:   "team",
+					Value: "estafette-team",
+				},
+				Label{
+					Key:   "language",
+					Value: "golang",
+				},
+			},
+			Manifest: "",
+			Commits: []GitCommit{
+				GitCommit{
+					Message: "First commit",
+					Author: GitAuthor{
+						Email:    "name@server.com",
+						Name:     "Name",
+						Username: "MyName",
+					},
+				},
+			},
+			InsertedAt: time.Date(2018, 4, 17, 8, 3, 0, 0, time.UTC),
+			UpdatedAt:  time.Date(2018, 4, 17, 8, 15, 0, 0, time.UTC),
 		})
 		pipelines = append(pipelines, &Pipeline{
 			ID:           "6",
@@ -61,16 +107,39 @@ func TestPipeline(t *testing.T) {
 			RepoRevision: "as23456",
 			BuildVersion: "1.0.0",
 			BuildStatus:  "succeeded",
-			Labels:       "",
-			Manifest:     "",
-			InsertedAt:   time.Date(2018, 4, 17, 8, 3, 0, 0, time.UTC),
-			UpdatedAt:    time.Date(2018, 4, 17, 8, 15, 0, 0, time.UTC),
+			Labels: []Label{
+				Label{
+					Key:   "app",
+					Value: "estafette-ci-api",
+				},
+				Label{
+					Key:   "team",
+					Value: "estafette-team",
+				},
+				Label{
+					Key:   "language",
+					Value: "golang",
+				},
+			},
+			Manifest: "",
+			Commits: []GitCommit{
+				GitCommit{
+					Message: "Second commit",
+					Author: GitAuthor{
+						Email:    "othername@server.com",
+						Name:     "Other Name",
+						Username: "OtherName",
+					},
+				},
+			},
+			InsertedAt: time.Date(2018, 4, 17, 8, 3, 0, 0, time.UTC),
+			UpdatedAt:  time.Date(2018, 4, 17, 8, 15, 0, 0, time.UTC),
 		})
 
 		// act
 		bytes, err := json.Marshal(&pipelines)
 
 		assert.Nil(t, err)
-		assert.Equal(t, "[{\"id\":\"5\",\"repoSource\":\"github.com\",\"repoOwner\":\"estafette\",\"repoName\":\"estafette-ci-api\",\"repoBranch\":\"master\",\"repoRevision\":\"as23456\",\"buildVersion\":\"1.0.0\",\"buildStatus\":\"succeeded\",\"labels\":\"\",\"manifest\":\"\",\"insertedAt\":\"2018-04-17T08:03:00Z\",\"updatedAt\":\"2018-04-17T08:15:00Z\"},{\"id\":\"6\",\"repoSource\":\"github.com\",\"repoOwner\":\"estafette\",\"repoName\":\"estafette-ci-api\",\"repoBranch\":\"master\",\"repoRevision\":\"as23456\",\"buildVersion\":\"1.0.0\",\"buildStatus\":\"succeeded\",\"labels\":\"\",\"manifest\":\"\",\"insertedAt\":\"2018-04-17T08:03:00Z\",\"updatedAt\":\"2018-04-17T08:15:00Z\"}]", string(bytes))
+		assert.Equal(t, "[{\"id\":\"5\",\"repoSource\":\"github.com\",\"repoOwner\":\"estafette\",\"repoName\":\"estafette-ci-api\",\"repoBranch\":\"master\",\"repoRevision\":\"as23456\",\"buildVersion\":\"1.0.0\",\"buildStatus\":\"succeeded\",\"labels\":[{\"key\":\"app\",\"value\":\"estafette-ci-api\"},{\"key\":\"team\",\"value\":\"estafette-team\"},{\"key\":\"language\",\"value\":\"golang\"}],\"manifest\":\"\",\"commits\":[{\"message\":\"First commit\",\"author\":{\"email\":\"name@server.com\",\"name\":\"Name\",\"username\":\"MyName\"}}],\"insertedAt\":\"2018-04-17T08:03:00Z\",\"updatedAt\":\"2018-04-17T08:15:00Z\"},{\"id\":\"6\",\"repoSource\":\"github.com\",\"repoOwner\":\"estafette\",\"repoName\":\"estafette-ci-api\",\"repoBranch\":\"master\",\"repoRevision\":\"as23456\",\"buildVersion\":\"1.0.0\",\"buildStatus\":\"succeeded\",\"labels\":[{\"key\":\"app\",\"value\":\"estafette-ci-api\"},{\"key\":\"team\",\"value\":\"estafette-team\"},{\"key\":\"language\",\"value\":\"golang\"}],\"manifest\":\"\",\"commits\":[{\"message\":\"Second commit\",\"author\":{\"email\":\"othername@server.com\",\"name\":\"Other Name\",\"username\":\"OtherName\"}}],\"insertedAt\":\"2018-04-17T08:03:00Z\",\"updatedAt\":\"2018-04-17T08:15:00Z\"}]", string(bytes))
 	})
 }
