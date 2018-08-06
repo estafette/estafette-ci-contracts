11bc9416c6a60abd4b82138deb09db5f982a1f97
Jorrit Salverda <jorrit.salverda@gmail.com>
2018-08-06 16:48:13 +0200

    add releases to pipeline

--- pipeline.go
+++ pipeline.go
@@ -13,6 +13,7 @@ type Pipeline struct {
 	BuildVersion string      `json:"buildVersion"`
 	BuildStatus  string      `json:"buildStatus"`
 	Labels       []Label     `json:"labels"`
+	Releases     []Release   `json:"releases"`
 	Manifest     string      `json:"manifest"`
 	Commits      []GitCommit `json:"commits"`
 	InsertedAt   time.Time   `json:"insertedAt"`
