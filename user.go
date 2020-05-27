a6d39e1f4c4a0122dd6e57b3135a4ab5d6b817a7
Jorrit Salverda <jsalverda@travix.com>
2020-05-27 13:37:45 +0200

    add first and last visit to user; add provider to user identity and group to replace source

--- user.go
+++ user.go
@@ -1,5 +1,7 @@
 package contracts
 
+import "time"
+
 // User represents a user of Estafette
 type User struct {
 	ID         string         `json:"id"`
@@ -7,17 +9,21 @@ type User struct {
 	Active     bool           `json:"active,omitempty"`
 	Identities []UserIdentity `json:"identities,omitempty"`
 	Groups     []UserGroup    `json:"groups,omitempty"`
+	FirstVisit time.Time      `json:"firstVisit,omitempty"`
+	LastVisit  time.Time      `json:"lastVisit,omitempty"`
 }
 
 // UserIdentity represents the various identities a user can have in different systems
 type UserIdentity struct {
 	Source   string `json:"source,omitempty"`
+	Provider string `json:"provider,omitempty"`
 	Username string `json:"username,omitempty"`
 	Email    string `json:"email,omitempty"`
 }
 
 // UserGroup represents a group of users as configured in different systems
 type UserGroup struct {
-	Source string `json:"source,omitempty"`
-	Name   string `json:"name,omitempty"`
+	Source   string `json:"source,omitempty"`
+	Provider string `json:"provider,omitempty"`
+	Name     string `json:"name,omitempty"`
 }
