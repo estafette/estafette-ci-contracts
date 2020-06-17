6248145608e9afd7cbc0675e89758b06e6193953
Jorrit Salverda <jsalverda@travix.com>
2020-06-17 14:21:41 +0200

    fix json field names for catalog entity

--- catalogEntity.go
+++ catalogEntity.go
@@ -5,13 +5,13 @@ import "time"
 // CatalogEntity represents any entity stored in the catalog tree
 type CatalogEntity struct {
 	ID             string                 `json:"id,omitempty"`
-	ParentKey      string                 `json:"parent_key,omitempty"`
-	ParentValue    string                 `json:"parent_value,omitempty"`
-	Key            string                 `json:"entity_key,omitempty"`
-	Value          string                 `json:"entity_value,omitempty"`
-	LinkedPipeline string                 `json:"linked_pipeline,omitempty"`
+	ParentKey      string                 `json:"parentKey,omitempty"`
+	ParentValue    string                 `json:"parentValue,omitempty"`
+	Key            string                 `json:"key,omitempty"`
+	Value          string                 `json:"value,omitempty"`
+	LinkedPipeline string                 `json:"linkedPipeline,omitempty"`
 	Labels         []Label                `json:"labels,omitempty"`
-	Metadata       map[string]interface{} `json:"entity_metadata,omitempty"`
+	Metadata       map[string]interface{} `json:"metadata,omitempty"`
 	InsertedAt     *time.Time             `json:"insertedAt,omitempty"`
 	UpdatedAt      *time.Time             `json:"updatedAt,omitempty"`
 }
