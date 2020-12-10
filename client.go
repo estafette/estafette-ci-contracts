535668a341ff8961fe733aba0c78b7a762b501e9
Jorrit Salverda <jsalverda@travix.com>
2020-12-10 15:49:29 +0100

    add organizations to client

--- client.go
+++ client.go
@@ -4,11 +4,12 @@ import "time"
 
 // Client represents a client application registered with Estafette
 type Client struct {
-	ID           string     `json:"id,omitempty"`
-	Active       bool       `json:"active,omitempty"`
-	Name         string     `json:"name,omitempty"`
-	ClientID     string     `json:"clientID,omitempty"`
-	ClientSecret string     `json:"clientSecret,omitempty"`
-	Roles        []*string  `json:"roles,omitempty"`
-	CreatedAt    *time.Time `json:"createdAt,omitempty"`
+	ID            string          `json:"id,omitempty"`
+	Active        bool            `json:"active,omitempty"`
+	Name          string          `json:"name,omitempty"`
+	ClientID      string          `json:"clientID,omitempty"`
+	ClientSecret  string          `json:"clientSecret,omitempty"`
+	Roles         []*string       `json:"roles,omitempty"`
+	Organizations []*Organization `json:"organizations,omitempty"`
+	CreatedAt     *time.Time      `json:"createdAt,omitempty"`
 }
